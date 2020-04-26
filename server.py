9ee1a26e9a5a84308f2d7697d9904fb2c8c8a192
omar <omaraparicio07@gmail.com>
2020-04-26 18:37:01 -0500

    #1 Alinenado el login al del pryecto nuxt y regresar el token de manera correcta

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify, request
-import os
+from flask import Flask, jsonify, request, make_response
+from flask_cors import CORS
 import jwt
+import os
+import base64
 import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile(os.path.join(".", "config/app.conf"), silent=False)
+CORS(app)
 
 @app.route("/", methods=['GET'])
 def greet():
@@ -16,26 +19,24 @@ def greet():
 @app.route("/login", methods=['POST'])
 def login():
   auth = request.get_json()
-  if app.config.get('LOGIN_USERNAME') == auth['user']and app.config.get('LOGIN_PASSWORD') == auth['password']:
+  if app.config.get('LOGIN_EMAIL') == auth['email']and app.config.get('LOGIN_PASSWORD') == auth['password']:
     # Tiempo de expiracion del token de 30 minutos
     expirationToken = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+
     payloadToken  = {
-      'user' : auth['user'],
+      'user' : auth['email'],
       'exp' : expirationToken
     }
-    print(payloadToken)
     token = jwt.encode(payloadToken, app.config['SECRET_KEY'])
-    print(token.decode('UTF-8'))
+
     response = {
-      "auth":True,
+      "email":auth['email'],
+      "username":auth['email'] + ' ' + auth['password'],
       "token":token.decode('UTF-8')
     }
-  else :
-    response={
-      "response":"Usuario no autorizado"
-    }
+    return jsonify(response)
 
-  return jsonify(response)
+  return make_response({'response':'Usuario no encontrado'}, 401)
 
 
 if __name__=='__main__':
