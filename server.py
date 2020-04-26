4a8901fe8e21d16f4af29b759c79c10762f47591
omar <omaraparicio07@gmail.com>
2020-04-26 16:08:59 -0500

    #1 Agregando token de retorno si el usuario y contraseña es correcto,aádiendo  secret key al archivo de configuracion para cifrar el token

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify, request
 import os
+import jwt
+import datetime
 
 app = Flask(__name__)
 app.config.from_pyfile(os.path.join(".", "config/app.conf"), silent=False)
@@ -15,13 +17,22 @@ def greet():
 def login():
   auth = request.get_json()
   if app.config.get('LOGIN_USERNAME') == auth['user']and app.config.get('LOGIN_PASSWORD') == auth['password']:
-
+    # Tiempo de expiracion del token de 30 minutos
+    expirationToken = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+    payloadToken  = {
+      'user' : auth['user'],
+      'exp' : expirationToken
+    }
+    print(payloadToken)
+    token = jwt.encode(payloadToken, app.config['SECRET_KEY'])
+    print(token.decode('UTF-8'))
     response = {
-      "auth":True
+      "auth":True,
+      "token":token.decode('UTF-8')
     }
   else :
     response={
-      "auth":"Usuario no autorizado"
+      "response":"Usuario no autorizado"
     }
 
   return jsonify(response)
