6face568310fb319da68effc745935679eaf1fd3
omar <omaraparicio07@gmail.com>
2020-04-26 15:42:22 -0500

    #1 Agregando endpoint login y validacion de usuario autorizado simple

--- server.py
+++ server.py
@@ -1,13 +1,29 @@
-from flask import Flask,jsonify
-
+from flask import Flask, jsonify, request
+import os
 
 app = Flask(__name__)
+app.config.from_pyfile(os.path.join(".", "config/app.conf"), silent=False)
 
 @app.route("/", methods=['GET'])
 def greet():
   response = {
     'response':"Hello!"
     }
+  return jsonify(response)
+
+@app.route("/login", methods=['POST'])
+def login():
+  auth = request.get_json()
+  if app.config.get('LOGIN_USERNAME') == auth['user']and app.config.get('LOGIN_PASSWORD') == auth['password']:
+
+    response = {
+      "auth":True
+    }
+  else :
+    response={
+      "auth":"Usuario no autorizado"
+    }
+
   return jsonify(response)
 
 
