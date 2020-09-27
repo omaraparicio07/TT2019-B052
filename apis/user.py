1bd8ba4b30560aae3498cfa8bddc7b5ee70b161e
omar <omar@makingdevs.com>
2020-09-27 01:46:26 -0500

    coding #1 Agregando metodo para crear usuarios a la intefaz swagger

--- apis/user.py
+++ apis/user.py
@@ -1,12 +1,17 @@
 from flask_restplus import Namespace, Resource, fields
+from flask import current_app
+from werkzeug.security import generate_password_hash, check_password_hash
 from config.database import db
 from bson import json_util, objectid
+from util import send_email, validate_email
+import datetime
+import jwt
 
 api = Namespace('user', description='Cats related operations')
 
 user = api.model('User', {
-  '_id': fields.String,
-  'name': fields.String,
+  '_id': fields.String(required=False, readonly=True),
+  'name': fields.String(required=True, readonly=True),
   'username': fields.String,
   'email': fields.String(required=True),
   'password': fields.String(required=True),
@@ -15,12 +20,52 @@ user = api.model('User', {
 
 
 @api.route('/')
-class CatList(Resource):
-  @api.doc('list_cats')
+class User(Resource):
   @api.marshal_list_with(user)
   def get(self):
-    '''List all cats'''
+    '''List all users'''
     users = db.db.users.find()
     response = list(users)
     return response
+  
+  @api.doc(responses={ 200: 'OK', 400: 'Faltan algunos argumentos', 500: 'Error en el servidor' } )
+  @api.expect( user )
+  def post(self):
+    '''Crear un nuevo usuario'''
+    print(api.payload)
+    # data = api.payload
+    # email = data['email']
+    # name = data['name']
+    # password = data['password']
+    users = db.db.users
+    existing_user = users.find_one({'email' : email})
 
+    print("---------"*10)
+    if existing_user:
+      api.abort(500, status = "El email del usuario ya se encuentra registrado", statusCode = "409")
+    
+    if validate_email(email) and name and password:
+      password_hashed = generate_password_hash(password)
+      idUser = db.db.users.insert_one({
+          'name':name,
+          'email': email,
+          'password': password_hashed,
+          'diagram':{}
+          })
+      if idUser:
+        expirationToken = datetime.datetime.utcnow() + datetime.timedelta(days=1)
+        payloadToken  = {
+          'user' : email,
+          'exp' : expirationToken
+          }
+
+      token = jwt.encode(payloadToken, current_app.config['SECRET_KEY'])
+      response = {
+        "email":email,
+        "name": name,
+        "token":token.decode('UTF-8')
+      }
+      email_result = send_email(email, password, name)
+      return response
+    else:
+      api.abort(500, status = "Ocurrio un error en el proceso", statusCode = "500")
